85988119128f0909194f060e84c2a5adfa06a6aa
Joeri van der Heijden <javdh74@xs4all.nl>
2017-11-14 21:47:14 +0100

    Least Common Multiple
    Least Common Multiple

---
+++ LeastCommonMultple/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LeastCommonMultple {
+	class Program {
+		static void Main(string[] args) {
+			const int a = 252, b = 105;
+			Console.WriteLine("Least Common Multiple of {0} and {1} = {2}", a, b, lcm(a, b));
+		}
+		static int gcd(int a, int b) {
+			if (b == 0)
+				return a;
+			else
+				return gcd(b, a % b);
+		}
+		static int lcm(int a, int b) {
+			return a * b / gcd(a, b);
+		}
+	}
+}
