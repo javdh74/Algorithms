c1c9b1a6268d927e1a1f714ba22dcde6cafeaf5c
Joeri van der Heijden <javdh74@xs4all.nl>
2017-11-14 21:46:26 +0100

    Insertion Sort
    Insertion Sort

---
+++ InsertionSort/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace InsertionSort {
+	class Program {
+		static void Main(string[] args) {
+			Increasing();
+			Decreasing();
+		}
+		static void Increasing() {
+			int[] A = new int[] { 5, 2, 4, 6, 1, 3 };
+			int j = 0, key = 0;
+			for (int i = 1; i < A.Length; i++) {
+				key = A[i];
+				j = i - 1;
+				while (j >= 0 && A[j] > key) {
+					A[j + 1] = A[j];
+					j--;
+				}
+				A[j + 1] = key;
+			}
+			for (int i = 0; i < A.Length; i++)
+				Console.Write("{0} ", A[i]);
+			Console.WriteLine();
+		}
+		static void Decreasing() {
+			int[] A = new int[] { 5, 2, 4, 6, 1, 3 };
+			int j = 0, key = 0;
+			for (int i = 1; i < A.Length; i++) {
+				key = A[i];
+				j = i - 1;
+				while (j >= 0 && A[j] < key) {
+					A[j + 1] = A[j];
+					j--;
+				}
+				A[j + 1] = key;
+			}
+			for (int i = 0; i < A.Length; i++)
+				Console.Write("{0} ", A[i]);
+			Console.WriteLine();
+		}			
+	}
+}
