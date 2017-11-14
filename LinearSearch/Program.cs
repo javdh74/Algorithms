c975a9d48df3f7d115a43a4f8db787a3be679871
Joeri van der Heijden <javdh74@xs4all.nl>
2017-11-14 21:47:48 +0100

    Linear Search
    Linear Search

---
+++ LinearSearch/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LinearSearch {
+	class Program {
+		static void Main(string[] args) {
+			const int value = 9;
+			int index = 0;
+			int[] sequence = new int[] { 5, 2, 4, 6, 1, 3 };
+			index = LinearSearch(sequence, value);
+			if (index == sequence.Length)
+				Console.WriteLine("Value not found");
+			else
+				Console.WriteLine(index);			
+		}
+		static int LinearSearch(int[] sequence, int value) {
+			int index = 0;
+			for (int i = 0; i < sequence.Length; i++) {
+				index = i;
+				if (sequence[index] == value)
+					return index;
+			}
+			return ++index;
+		}
+	}
+}
