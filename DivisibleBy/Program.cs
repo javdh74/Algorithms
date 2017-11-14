2b6c0e2d74b06d486375510899f0e8ca0e6294af
Joeri van der Heijden <javdh74@xs4all.nl>
2017-11-14 21:43:52 +0100

    Divisible By
    Divisible By

---
+++ DivisibleBy/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Numerics;
+
+namespace DivisibleBy {
+	class Program {
+		static void Main(string[] args) {
+			Console.Write("Enter numerator: ");
+			BigInteger numerator = BigInteger.Parse(Console.ReadLine());
+			Console.Write("Enter denominator: ");
+			BigInteger denominator = BigInteger.Parse(Console.ReadLine());
+			if (DivisibleBy(numerator, denominator))
+				Console.WriteLine("{0} is divisible by {1}", numerator, denominator);
+			else
+				Console.WriteLine("{0} is not divisible by {1}", numerator, denominator);
+		}
+		static bool DivisibleBy(BigInteger n, BigInteger d) {
+			if (n % d == 0)
+				return true;
+			else
+				return false;
+		}
+	}
+}
