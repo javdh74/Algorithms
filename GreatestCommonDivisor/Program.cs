56cc7a97d4ba4c0bd250e42b4a51be324af31d1f
Joeri van der Heijden <javdh74@xs4all.nl>
2017-11-14 21:45:35 +0100

    Greatest Common Divisor
    Greatest Common Divisor

---
+++ GreatestCommonDivisor/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GreatestCommonDivisor {
+	class Program {
+		static void Main(string[] args) {
+			const int a = 252, b = 105;
+			Console.WriteLine("Greatest Common Divisor of {0} and {1} = {2}", a, b, gcdi(a, b));
+			Console.WriteLine("Greatest Common Divisor of {0} and {1} = {2}", a, b, gcdr(a, b));
+		}
+		static int gcdi(int a, int b) {
+			while (a != b)
+				if (a > b)
+					a -= b;
+				else
+					b -= a;
+			return a;
+		}
+		static int gcdr(int a, int b) {
+			if (b == 0)
+				return a;
+			else
+				return gcdr(b, a % b);
+		}
+	}
+}
