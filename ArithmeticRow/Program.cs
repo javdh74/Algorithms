5836673410c7c70840b77f8e3279711fd0fb6bf7
Joeri van der Heijden <javdh74@xs4all.nl>
2017-11-14 21:43:17 +0100

    Arithmetic Row
    Arithmetic Row

---
+++ ArithmeticRow/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ArithmeticRow {
+	class Program {
+		static void Main(string[] args) {
+			const double a = 1, n = 4, r = 2;	// r != 1
+			Console.WriteLine(ArithmeticRow(a, n, r));
+		}
+		static double ArithmeticRow(double a, double n, double r) {
+			return (a * (1 - Math.Pow(r, n))) / (1 - r);
+		}
+	}
+}
