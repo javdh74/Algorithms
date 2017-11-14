eb3698d3d0714d1271950cc2fae9d58a2bf7deee
Joeri van der Heijden <javdh74@xs4all.nl>
2017-11-14 21:44:55 +0100

    Geometric Row
    Geometric Row

---
+++ GeometricRow/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GeometricRow {
+	class Program {
+		static void Main(string[] args) {
+			const double a = 1.0, r = 0.5;	// -1 < r < 1
+			Console.WriteLine(GeometicRow(a, r));
+		}
+		static double GeometicRow(double a, double r) {
+			return a / (1 - r);
+		}
+	}
+}
