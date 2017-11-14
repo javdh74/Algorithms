bc281c2c590644eea624fc7e51b35cc3c0a1d691
Joeri van der Heijden <javdh74@xs4all.nl>
2017-11-14 21:44:24 +0100

    Faculty
    Faculty

---
+++ Faculty/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Numerics;
+
+namespace Faculty {
+	class Program {
+		static void Main(string[] args) {
+			const int limit = 100;
+			Console.WriteLine(Faculty(limit));
+		}
+		static BigInteger Faculty(int n) {
+			if (n < 0)
+				return 0;
+			BigInteger product = 1;
+			for (int j = 1; j <= n; j++)
+				product *= j;
+			return product;
+		}
+	}
+}
