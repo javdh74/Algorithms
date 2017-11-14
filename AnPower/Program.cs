0a339e87f1c5380a016e1e621f8f129ffd07987e
Joeri van der Heijden <javdh74@xs4all.nl>
2017-11-14 21:42:48 +0100

    An Power
    An Power

---
+++ AnPower/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AnPower {
+	class Program {
+		static void Main(string[] args) {
+			const double exponent = 1000.1;
+			const int an = 100;
+			Console.WriteLine((decimal)Math.Pow(an, exponent));
+		}
+	}
+}
