34f762c88f797a6c0727ff7fa1c3b7d455b96969
Joeri van der Heijden <javdh74@xs4all.nl>
2017-11-14 21:48:33 +0100

    RPower
    RPower

---
+++ RPower/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace RPower {
+	class Program {
+		static void Main(string[] args) {
+			const double r = 9.99;
+			const int exponent = 1001;
+			Console.WriteLine((decimal)Math.Pow(r, exponent));
+		}
+	}
+}
